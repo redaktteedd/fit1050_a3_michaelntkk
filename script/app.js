735bc0be6d68b7e3bc6494fd3944a00d4d8cd799
michaelntk <novantomichael1@gmail.com>
2024-05-26 22:08:04 +1000

    added review and hero section

---
+++ script/app.js
@@ -0,0 +1,39 @@
+// add classes for mobile navigation toggling
+var CSbody = document.querySelector('body');
+const CSnavbarMenu = document.querySelector('#cs-navigation');
+const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+
+//script to change the navbar color when scrolled
+function handleScroll() {
+    const navigation = document.getElementById('cs-navigation');
+	// debug purpose 
+    //console.log('Scrolling...');
+    if (window.scrollY > 100) {
+        navigation.classList.add('scrolled');
+    } else {
+        navigation.classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+function ariaExpanded() {
+	const csUL = document.querySelector('#cs-expanded');
+	const csExpanded = csUL.getAttribute('aria-expanded');
+
+	if (csExpanded === 'false') {
+		csUL.setAttribute('aria-expanded', 'true');
+	} else {
+		csUL.setAttribute('aria-expanded', 'false');
+	}
+}
+
+// mobile nav toggle code
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+for (const item of dropDowns) {
+	const onClick = () => {
+		item.classList.toggle('cs-active');
+	};
+	item.addEventListener('click', onClick);
+}
